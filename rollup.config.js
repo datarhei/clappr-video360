270ce94e37e8053251eca4db0dd11628f5c2e6f7
Ingo Oppermann <ingo@oppermann.ch>
2020-04-30 15:30:12 +0200

    Integrate fixed kaleidoscope version

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,16 @@
-import babel from 'rollup-plugin-babel';
-import commonjs from 'rollup-plugin-commonjs';
-import nodeResolve from 'rollup-plugin-node-resolve';
-import strip from 'rollup-plugin-strip';
+import babel from '@rollup/plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
+import nodeResolve from '@rollup/plugin-node-resolve';
 
+const name = 'Video360';
+const output = [
+    {file: 'dist/clappr-video360.js', format: 'umd', name, globals: {'clappr': 'Clappr'}}
+];
 
 export default {
-  entry: 'src/index.js',
+  input: 'src/index.js',
+  output,
   external: ['clappr'],
-  globals: {'clappr': 'Clappr'},
   acorn: {
     allowReserved: true,
   },
@@ -16,11 +19,7 @@ export default {
       jsnext: true,
       browser: true,
     }),
-    strip({debugger: true, sourceMap: false}),
     commonjs(),
-    babel(),
-  ],
-  output: {
-    file: 'dist/clappr-video360.js', format: 'umd', name: 'Video360'
-  },
+    babel({ babelHelpers: 'inline' }),
+  ]
 };
